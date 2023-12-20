737bd0bb2538ab9885a5730636db85f718fab2d0
ShivamYamini <shivamyamini07@gmail.com>
2023-12-20 21:23:06 +0530

    Initial Commit

---
+++ Employee-REST-API/src/main/java/com/employee/employee/EmployeeApplication.java
@@ -0,0 +1,14 @@
+package com.employee.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplication {
+	//http://localhost:8080/swagger-ui/index.html#/
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApplication.class, args);
+	}
+
+}
